5ea1a3327f422505418670717124458c5e0af6d5
Goodness Igunma <Goodnessigunma1@gmail.com>
2021-09-27 12:13:22 +0100

    About to add the vue error messages

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -72,7 +72,7 @@
           </form>
           <!-- Registration Form -->
           <vee-form v-show="tab === 'register'" :validation-schema="schema"
-            @submit="register">
+            @submit="register" :initial-values="userData">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -88,47 +88,59 @@
               <vee-field type="email" name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email" />
+                  placeholder="Enter Email" />
                 <ErrorMessage class="text-red-600" name="email"></ErrorMessage>
             </div>
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input type="number"
+              <vee-field type="number" name="age"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded" />
+                  <ErrorMessage class="text-red-600" name="age"></ErrorMessage>
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input type="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" />
+                placeholder="Password" v-bind="field" />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+                </vee-field>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input type="password"
+              <vee-field type="password" name="confirm_password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password" />
+                <ErrorMessage class="text-red-600" name="confirm_password"></ErrorMessage>
             </div>
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
-              <select
+              <vee-field as="select" name="country"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded">
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
-              </select>
+                <option value="Nigeria">Nigeria</option>
+              </vee-field>
+              <ErrorMessage class="text-red-600" name="country"></ErrorMessage>
+
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
+              <vee-field type="checkbox" name="tos" value="1"
+              class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
               <label class="inline-block">Accept terms of service</label>
+              <ErrorMessage class="text-red-600" name="tos"></ErrorMessage>
+
             </div>
             <button type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
@@ -145,7 +157,6 @@
 <script>
 import { mapMutations, mapState } from 'vuex';
 
-
 export default {
     name: 'Auth',
 
@@ -156,14 +167,18 @@ export default {
         schema: {
           name: 'required|min:3|max:100|alpha_spaces',
           email: 'required|min:3|max:100|email',
-          age: '',
-          password: '',
-          confirm_password: '',
-          country: '',
-          tos: '',
-        }
+          age: 'required|minVal:18|maxVal:100',
+          password: 'required|min:4|max:100',
+          confirm_password: 'confirmed:@password',
+          country: 'required|excluded:Nigeria',
+          tos: 'required',
+        },
+
+        userData: {
+          country: 'USA',
+        },
     };
-    },
+  },
 
     computed: {
       // authModalShow(){
@@ -175,7 +190,10 @@ export default {
 
     methods: {
       ...mapMutations(['toggleAuthModal']),
-    }
+      register(values){
+        console.log(values);
+      },
+    },
 
-}
+};
 </script>
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,8 +1,9 @@
 import {
-    Form as VeeForm, Field as VeeField, defineRule, ErrorMessage,
+    Form as VeeForm, Field as VeeField, defineRule, ErrorMessage, configure,
   } from 'vee-validate';
   import {
     required, min, max, alpha_spaces as alpaSpaces, alpha_spaces, email,
+    min_value as minVal, max_value as maxVal, confirmed, not_one_of as excluded,
   } from '@vee-validate/rules';
 
 export default{
@@ -17,5 +18,11 @@ export default{
         defineRule('max', max);
         defineRule('alpha_spaces', alpha_spaces);
         defineRule('email', email);
+
+        // The way i define minimium & maximium value is different from the video
+        defineRule('minVal', minVal);
+        defineRule('maxVal', maxVal);
+        defineRule('confirmed', confirmed);
+        defineRule('excluded', excluded);
     },
 };
